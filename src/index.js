18f65de2b549d4a65bd00c7930ae639d2df69914
Giorgix <jorgemdramos@gmail.com>
2019-12-09 19:57:38 +0100

    feat() added interval test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interval(1000).pipe(
+    map(value => ({checking: value, savings: value}))
+).subscribe(props =>
+    ReactDOM.render(
+        <App {...props}/>,
+        document.getElementById('root')
+    )
+);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
