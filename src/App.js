43e0ce8751e334af13e296b5fdb73ce8b4690a9f
Giorgix <jorgemdramos@gmail.com>
2019-12-09 19:57:23 +0100

    feat() added function component test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const AccountBalance = props => {
+  return(
+    <div className="account-info">
+      <p>Checking {props.checking}</p>
+      <p>Savings {props.savings}</p>
+    </div>
+  );
+}
+
+const App = props => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AccountBalance {...props}/>
       </header>
     </div>
   );
