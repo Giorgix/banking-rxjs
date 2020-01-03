5292c533d695d34cf16c852cc0bb8f65cc43bd66
Jorge <jorgemdramos@gmail.com>
2020-01-03 19:42:00 +0100

    feat() replaced Class and state for hooks and function component

--- src/components/Balances/index.js
+++ src/components/Balances/index.js
@@ -1,42 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import AccountBalance from '../AccountBalance';
 import { distinctUntilChanged, pluck } from 'rxjs/operators';
 
 // Creates a composite class for both accounts
-export default class Balances extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checking: 0,
-      savings: 0,
-    };
-  }
+export default function Balances (props) {
+  const [checking, setChecking] = useState(0);
+  const [savings, setSavings] = useState(0);
 
-  componentDidMount() {
-
-    // Subscribes to updates on the balances
-    this.props.appState$.pipe(
+  useEffect(() => {
+    props.appState$.pipe(
       distinctUntilChanged('accounts'),
       pluck('accounts'),
     ).subscribe(({checking, savings}) => {
-      this.setState({checking, savings})
+      setChecking(checking);
+      setSavings(savings);
     });
-  }
+  })
 
-  onChange(e) {
+  function onChange(e) {
     console.log('change input... ', e.target.value);
-    
   }
 
-  render() {
-    const { checking, savings } = this.state;
-
-    // Renders the component as the composition of two subcomponents
-    return(
-      <div className="account-info row">
-        <AccountBalance onChange={this.onChange} {...this.props} name="Checking" value={checking} />
-        <AccountBalance onChange={this.onChange} {...this.props} name="Savings" value={savings} />
-      </div>
-    )
-  }
+  // Renders the component as the composition of two subcomponents
+  return(
+    <div className="account-info row">
+      <AccountBalance onChange={onChange} {...props} name="Checking" value={checking} />
+      <AccountBalance onChange={onChange} {...props} name="Savings" value={savings} />
+    </div>
+  )
 }
