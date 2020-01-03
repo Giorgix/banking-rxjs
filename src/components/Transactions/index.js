30da6e4faa43472d0c6d093b36731c765bf18033
Jorge <jorgemdramos@gmail.com>
2020-01-03 19:42:08 +0100

    feat() replaced Class and state for hooks and function component

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -1,71 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import Transaction from '../Transaction';
 import { distinctUntilChanged, pluck } from 'rxjs/operators';
 
-// Creates a composite class for both accounts
-export default class Transactions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transactions: []
-    };
-  }
+// Creates a function component to hold the transactions state from the redux store
+const Transactions = props => {
+  const [transactions, setTransactions] = useState([]);
 
-  componentDidMount() {
-
+  useEffect(() => {
     // Subscribes to updates on the balances
-    this.props.appState$.pipe(
+    props.appState$.pipe(
       distinctUntilChanged('transactions'),
       pluck('transactions'),
     ).subscribe(transactions => {
-      this.setState({transactions})
+      setTransactions(transactions)
     });
-  }
+  })
 
-  isWithdraw(transaction) {
+  function isWithdraw(transaction) {
     return transaction.type === 'WITHDRAW';
   }
 
-  parseAmount(transaction) {
-    return this.isWithdraw(transaction) ? '-' + transaction.amount.toFixed(2) : '+' + transaction.amount.toFixed(2);
+  function parseAmount(transaction) {
+    return isWithdraw(transaction) ? '-' + transaction.amount.toFixed(2) : '+' + transaction.amount.toFixed(2);
   }
 
-  renderItems() {
-    return this.state.transactions.map((transaction, index) => {
+  function renderItems() {
+    return transactions.map((transaction, index) => {
       return (
         <tr key={transaction.timestamp}>
           <td>{index + 1}</td>
-          <td className="valign-wrapper"><strong>{transaction.type}</strong> {this.isWithdraw(transaction)
+          <td className="valign-wrapper"><strong>{transaction.type}</strong> {isWithdraw(transaction)
             ? <i className="material-icons red-text text-darken-4">arrow_back</i>
             : <i className="material-icons green-text">arrow_forward</i>}</td>
           <td>{new Date(transaction.timestamp).toLocaleString('es-es')}</td>
           <td>{transaction.account}</td>
-          <td><strong>{this.parseAmount(transaction)}</strong></td>
+          <td><strong>{parseAmount(transaction)}</strong></td>
           <td>{transaction.balance.toFixed(2)}</td>
         </tr>
       );
     })
   }
 
-  render() {
+  // Renders the component as the composition of two subcomponents
+  return(
+    <table className="responsive-table striped">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Type</th>
+        <th>Date</th>
+        <th>Account</th>
+        <th>Amount</th>
+        <th>Current Balance</th>
+      </tr>
+    </thead>
+    <tbody>
+      {renderItems()}
+    </tbody>
+    </table>
+  )
+};
 
-    // Renders the component as the composition of two subcomponents
-    return(
-      <table className="responsive-table striped">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Type</th>
-          <th>Date</th>
-          <th>Account</th>
-          <th>Amount</th>
-          <th>Current Balance</th>
-        </tr>
-      </thead>
-      <tbody>
-        {this.renderItems()}
-      </tbody>
-      </table>
-    )
-  }
-}
+export default Transactions;
