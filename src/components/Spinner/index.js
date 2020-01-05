87182f758de93206731500e638b0761bd7242ebc
Jorge <jorgemdramos@gmail.com>
2020-01-05 18:11:28 +0100

    feat() added spinner component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+export default () =>
+<div className="spinner">
+    <div className="double-bounce1"></div>
+    <div className="double-bounce2"></div>
+</div>
