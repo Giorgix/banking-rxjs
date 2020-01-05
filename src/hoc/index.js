a16a2755a4fb3ee072b786e61b94d069b5759c3e
Jorge <jorgemdramos@gmail.com>
2020-01-05 18:23:04 +0100

    doc() added reference to hocs

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,3 +1,6 @@
+/**
+ * HOCs inspired in the project https://github.com/madoos/real-world-fp-js
+ */
 export {default as branch} from './branch';
 export {default as fetch} from './fetch';
 export {default as withStoreState} from './withStoreState';
