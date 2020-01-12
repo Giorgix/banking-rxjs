a8342f1a9beff7e727f37a57fbcad25ba293bc0d
Jorge <jorgemdramos@gmail.com>
2020-01-12 17:12:17 +0100

    refactor() compose Accounts balance through hocs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
 // Ramda
-import { compose, prop } from 'ramda';
+import { compose, prop, curry, pick } from 'ramda';
 
 // Components
-import Balances from './components/Balances';
+//import Balances from './components/Balances';
 import Transactions from './components/Transactions';
 import Spinner from './components/Spinner';
+import AccountBalance from './components/AccountBalance';
 
 // Redux
 import configureStore from './redux-store/store.js';
-import {withdraw, fetchAccounts} from './actions';
+import {fetchAccounts} from './actions';
 
 // Utils
 import createStreamFromStore from './utils/createStreamFromStore';
 
 // HOC
-import {branch, fetch, withStoreState} from './hoc';
+import {branch, withStoreState, toList, withDispatcher} from './hoc';
 
 // Assets
 import logo from './logo.svg';
@@ -25,21 +26,23 @@ import './App.css';
 const store = configureStore();
 const store$ = createStreamFromStore(store);
 store.dispatch(fetchAccounts());
-const action = withdraw({amount: 20, account: 'checking'});
-//store.dispatch(action);
 
-const parseResponse = data => ({
-  ...data,
+const withPickedProps = curry((propsToPick, BaseComponent) => props => {
+  const newProps = pick(propsToPick, props);
+  return <BaseComponent {...newProps} />;
 })
 
 const enhace = compose(
-  withStoreState(store$, store.dispatch),
+  withStoreState(store$),
+  withDispatcher(store.dispatch),
+  withPickedProps(['accounts', 'isFetching', 'dispatch', 'lastUpdated']),
   branch(prop('isFetching'), Spinner),
+  toList({className: 'accounts-list row'}),
 );
 
 //const BalancesOrSpinner = branch(prop('isFetching'), Spinner, Balances);
 //const WithStoreState = withStoreState(store$, BalancesOrSpinner);
-const Accounts = enhace(Balances);
+const Accounts = enhace(AccountBalance);
 
 const App = props => {
   // Renders the balances component to the DOM and passes the balance$ stream
