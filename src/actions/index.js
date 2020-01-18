08902d49e86e585c5c76d537a893d94daf7a5181
Jorge <jorgemdramos@gmail.com>
2020-01-18 13:13:14 +0100

    feat() added actions to work with redux-observable epics

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,16 +6,7 @@ export function requestAccounts (payload) {
     }
 }
 
-export const RECEIVE_ACCOUNTS = 'RECEIVE_ACCOUNTS';
-export function receiveAccounts (json) {
-    console.log('action dta: ', json);
-    
-    return {
-        type: RECEIVE_ACCOUNTS,
-        accounts: json.data,
-        receivedAt: Date.now()
-    }
-}
+export const RECEIVE_ACCOUNTS_FULLFILLED = 'RECEIVE_ACCOUNTS_FULLFILLED';
 
 // DEPOSIT
 export const DEPOSIT = 'DEPOSIT';
@@ -34,37 +25,3 @@ export function withdraw(payload){
         ...payload
     }
 }
-
-const delay = t => x => new Promise(resolve => setTimeout(() => resolve(x), t));
-
-// Though its insides are different, you would use it just like any other action creator:
-// store.dispatch(fetchPosts('reactjs'))
-export function fetchAccounts() {
-    // Thunk middleware knows how to handle functions.
-    // It passes the dispatch method as an argument to the function,
-    // thus making it able to dispatch actions itself.
-    return function(dispatch) {
-      // First dispatch: the app state is updated to inform
-      // that the API call is starting.
-      dispatch(requestAccounts())
-      // The function called by the thunk middleware can return a value,
-      // that is passed on as the return value of the dispatch method.
-      // In this case, we return a promise to wait for.
-      // This is not required by thunk middleware, but it is convenient for us.
-      return fetch(`http://localhost:3000/accounts`)
-        .then(delay(Math.trunc(Math.random() * (3000 - 350) + 350)))
-        .then(
-          response => response.json(),
-          // Do not use catch, because that will also catch
-          // any errors in the dispatch and resulting render,
-          // causing a loop of 'Unexpected batch number' errors.
-          // https://github.com/facebook/react/issues/6895
-          error => console.log('An error occurred.', error)
-        )
-        .then(json =>
-          // We can dispatch many times!
-          // Here, we update the app state with the results of the API call.
-          dispatch(receiveAccounts(json))
-        )
-    }
-  }
