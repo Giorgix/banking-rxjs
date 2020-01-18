ffda82320ce8d0eb83b9c9d7e1e2a355d77c2d96
Jorge <jorgemdramos@gmail.com>
2020-01-18 13:16:09 +0100

    feat() add enahceOperations and enhaceTransactions using hocs

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React from 'react';
 // Ramda
 import { compose, prop, curry, pick } from 'ramda';
 
+// RxJS
+import { distinctUntilChanged } from 'rxjs/operators';
+
 // Components
 //import Balances from './components/Balances';
 import Transactions from './components/Transactions';
 import Spinner from './components/Spinner';
 import AccountBalance from './components/AccountBalance';
+import ProductOperations from './components/ProductOperations';
 
 // Redux
 import configureStore from './redux-store/store.js';
-import {fetchAccounts} from './actions';
 
 // Utils
 import createStreamFromStore from './utils/createStreamFromStore';
@@ -25,37 +28,54 @@ import './App.css';
 
 const store = configureStore();
 const store$ = createStreamFromStore(store);
-store.dispatch(fetchAccounts());
+store.dispatch({type: 'REQUEST_ACCOUNTS'});
 
 const withPickedProps = curry((propsToPick, BaseComponent) => props => {
   const newProps = pick(propsToPick, props);
   return <BaseComponent {...newProps} />;
 })
 
-const enhace = compose(
-  withStoreState(store$),
-  withDispatcher(store.dispatch),
-  withPickedProps(['accounts', 'isFetching', 'dispatch', 'lastUpdated']),
+const enhaceAccount = compose(
+  withStoreState(store$, [distinctUntilChanged('accounts')]),
+  withPickedProps(['accounts', 'isFetching', 'lastUpdated']),
   branch(prop('isFetching'), Spinner),
   toList({className: 'accounts-list row'}),
 );
 
+const enhaceOperations = compose(
+  withStoreState(store$, [distinctUntilChanged('accounts')]),
+  withDispatcher(store.dispatch),
+  withPickedProps(['accounts', 'isFetching', 'dispatch', 'lastUpdated']),
+  branch(prop('isFetching'), Spinner),
+);
+
+const enhaceTransactions = compose(
+  withStoreState(store$, [distinctUntilChanged('transactions')]),
+  withDispatcher(store.dispatch),
+  withPickedProps(['transactions', 'isFetching', 'dispatch', 'lastUpdated']),
+  branch(prop('isFetching'), Spinner),
+);
+
 //const BalancesOrSpinner = branch(prop('isFetching'), Spinner, Balances);
 //const WithStoreState = withStoreState(store$, BalancesOrSpinner);
-const Accounts = enhace(AccountBalance);
-
+const Accounts = enhaceAccount(AccountBalance);
+const Operations = enhaceOperations(ProductOperations)
+const TransactionsEnhace = enhaceTransactions(Transactions);
 const App = props => {
   // Renders the balances component to the DOM and passes the balance$ stream
   // as props to populate the UI with the illusion of constant cash flow into both accounts
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
       </header>
       <main>
         <Accounts />
+        <div className="row">
+          <Operations />
+        </div>
         <div className="container">
-          <Transactions appState$={store$} dispatch={store.dispatch}/>
+          <TransactionsEnhace />
         </div>
       </main>
     </div>
