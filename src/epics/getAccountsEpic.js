f2d71b126d99fff5f8a06e6bd6f6d7356e111a41
Jorge <jorgemdramos@gmail.com>
2020-01-21 21:17:15 +0100

    feat() use firebase rxfire observables

--- src/epics/getAccountsEpic.js
+++ src/epics/getAccountsEpic.js
@@ -1,13 +1,16 @@
-import { ajax } from 'rxjs/ajax';
-import { map, switchMap, catchError, timestamp, delay } from 'rxjs/operators';
+import { map, switchMap, catchError, timestamp, delay, tap } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
+import { collectionData } from 'rxfire/firestore';
+import { db } from '../firebase';
+
+const accountsRef = db.collection('accounts');
 
 export default (action$, state$) => action$.pipe(
         ofType('REQUEST_ACCOUNTS'),
         delay(Math.trunc(Math.random() * (3000 - 500) + 500)),
         switchMap(action =>
-            ajax('http://localhost:3000/accounts').pipe(
-                map(responseObj => responseObj.response),
+            collectionData(accountsRef, 'id').pipe(
+                tap(console.log),
                 timestamp(),
                 map(obj => ({data: [...obj.value], timestamp: obj.timestamp})),
                 map(payload => ({
