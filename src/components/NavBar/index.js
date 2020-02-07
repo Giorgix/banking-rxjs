83ccd8c1f6948b400762b67019b37373d2501d56
Jorge <jorgemdramos@gmail.com>
2020-02-07 20:07:42 +0100

    feat() add logged in and logged out states

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,74 +1,120 @@
 import React, {useEffect} from 'react';
+import Link from 'next/link';
 import './NavBar.module.css';
 //import logo from '../../logo.svg';
-import {branch} from '../../hoc';
+import {branch, withConnectedActions} from '../../hoc';
 import {firebaseAuth} from '../../firebase';
 
 const Auth = branch(
   ({user}) => user && user.username,
-  ({user}) => {
+  ({user, logOut}) => {
+
+    useEffect(() => {
+      const dropdownElems = document.querySelectorAll('.dropdown-trigger');
+      const dropdownInstances = window.M.Dropdown.init(dropdownElems, {coverTrigger: false});
+
+      return () => {
+        dropdownInstances && dropdownInstances.forEach(instance => {
+          instance.destroy();
+        });
+      };
+    });
+
     return (
-      <a className="dropdown-trigger" href="#!" data-target="dropdown1">
-        <span className="username">{user.username}</span>
-        <img className="user-avatar" src={user.profile_picture} alt="user profile" />
-        <i className="material-icons right">arrow_drop_down</i>
-      </a>
+      <React.Fragment>
+        <ul id="dropdown1" className="dropdown-content">
+          <li><a href="#!">settings</a></li>
+          <li><a href="#!">notifications</a></li>
+          <li className="divider"></li>
+          <li><a href="#!" onClick={() => logOut()}>logout</a></li>
+        </ul>
+        <a className="dropdown-trigger" href="#!" data-target="dropdown1">
+          <span className="username">{user.username}</span>
+          <img className="user-avatar" src={user.profile_picture} alt="user profile" />
+          <i className="material-icons right">arrow_drop_down</i>
+        </a>
+      </React.Fragment>
     )
   },
-  ({authObject}) => {
+  () => {
     return (
-      <a className="waves-effect waves-light btn" onClick={authObject}>
-        Log in
-        <i className="material-icons right">account_circle</i>
-      </a>
+      <Link href="/login">
+        <a className="waves-effect waves-light btn">
+          Log in
+          <i className="material-icons right">account_circle</i>
+        </a>
+      </Link>
     )
   }
 )
 
-export default function NavBar ({user, authObject}) {
-  useEffect(() => {
-      const elems = document.querySelectorAll('.dropdown-trigger');
-      const instances = window.M.Dropdown.init(elems, {coverTrigger: false});
 
-      return () => {
-        instances && instances.forEach(instance => {
-          instance.destroy();
-        });
-      };
-    })
+function NavBar ({user, authObject, logOut}) {
 
+  const loggedNav = () => {
     return(
-      <>
-      <ul id="dropdown1" className="dropdown-content">
-        <li><a href="#!">settings</a></li>
-        <li><a href="#!">notifications</a></li>
-        <li className="divider"></li>
-        <li><a href="#!" onClick={() =>
-          firebaseAuth.signOut().then(() => console.log('LOGGED OUT!')
-          ).catch((error) => console.log('ERROR! ', error))}>logout</a></li>
-      </ul>
-      <nav>
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
+      <React.Fragment>
+        <li><Link href="/dashboard"><a>Dashboard</a></Link></li>
+        <li><a href="badges.html">My Products</a></li>
+        <li><a href="collapsible.html">Add product</a></li>
+      </React.Fragment>
+    )
+  };
+
+  const notLoggedNav = (user) => {
+    return(
+      <React.Fragment>
+        <li><a href="sass.html">About</a></li>
+        <li><a href="collapsible.html">Sign Up</a></li>
+      </React.Fragment>
+    )
+  };
+
+  const Menu = branch(
+    ({user}) => user && user.username,
+    ({user}) => loggedNav(user),
+    () => notLoggedNav()
+  );
+
+  useEffect(() => {
+    var sidenavElems = document.querySelectorAll('.sidenav');
+    var sidenavInstances = window.M.Sidenav.init(sidenavElems);
+
+    return () => {
+      sidenavInstances && sidenavInstances.forEach(instance => {
+        instance.destroy();
+      });
+    };
+  });
+
+  return(
+    <>
+    <nav>
+      <div className="nav-wrapper">
+        <Link href="/">
+          <a className="brand-logo">
             <img src="/logo.svg" className="navbar-logo" alt="logo" />
             <span>Banking RxJS</span>
           </a>
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-            <i className="material-icons">menu</i>
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Dashboard</a></li>
-            <li><a href="badges.html">My Products</a></li>
-            <li><a href="collapsible.html">Add product</a></li>
-            <li><Auth user={user} authObject={authObject}/></li>
-          </ul>
-        </div>
-      </nav>
-      <ul className="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Dashboard</a></li>
-        <li><a href="badges.html">My Products</a></li>
-        <li><a href="collapsible.html">Add product</a></li>
-      </ul>
-      </>
-    )
+        </Link>
+        <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+          <i className="material-icons">menu</i>
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <Menu user={user}/>
+          <li><Auth logOut={logOut} user={user} authObject={authObject}/></li>
+        </ul>
+      </div>
+    </nav>
+    <ul className="sidenav" id="mobile-demo">
+      <Menu user={user}/>
+      <li><a href="#!">settings</a></li>
+      <li><a href="#!">notifications</a></li>
+      <li className="divider"></li>
+      <li><a href="#!" onClick={() => logOut()}>logout</a></li>
+    </ul>
+    </>
+  )
 }
+
+export default withConnectedActions(['logOut'])(NavBar);
