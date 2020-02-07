a5cedef9d10e0230e8be9abcf4d259718a428687
Jorge <jorgemdramos@gmail.com>
2020-02-07 20:08:10 +0100

    feat() add Navigation with user state

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import NavBar from '../NavBar';
 import Head from 'next/head';
 
+// Firebase
+import { authState } from 'rxfire/auth';
+import {firebaseAuth} from '../../firebase';
+
+// Redux
+import { connect } from 'react-redux';
+import {requestAccounts, startInterest, stopInterest} from '../../actions';
+
+import { compose, prop, curry } from 'ramda';
+
+// HOC
+import {withUserState} from '../../hoc';
+
 const layoutStyle = {
   margin: 20,
   padding: 20,
   border: '1px solid #DDD'
 };
+
+const enhace = compose(
+  withUserState(authState(firebaseAuth)),
+);
+
+const Navigation = enhace(NavBar);
 
 const Layout = props => (
   <>
@@ -15,7 +34,7 @@ const Layout = props => (
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
     <script src="/materialize.min.js" />
   </Head>
-  <NavBar />
+  <Navigation />
   <div style={layoutStyle}>
     {props.children}
   </div>
