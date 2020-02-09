e0aa2ca238267e123c7ad438d9e89e4dabd77e92
Jorge <jorgemdramos@gmail.com>
2020-02-09 14:06:33 +0100

    feat() use connectedProp for user state from store

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,13 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import NavBar from '../NavBar';
 import Head from 'next/head';
 
 // Utils
 import { compose } from 'ramda';
 
-import { AuthUserContext } from '../Session';
 
 // HOC
-import {withConnectedProps, withUserState, withAuthentication} from '../../hoc';
+import { withConnectedProps } from '../../hoc';
 
 const layoutStyle = {
   margin: 20,
@@ -16,29 +15,26 @@ const layoutStyle = {
   border: '1px solid #DDD'
 };
 
-/*const enhace = compose(
-  withUserState
+const enhace = compose(
+  withConnectedProps(['user'])
 );
 
-const Navigation = enhace(NavBar);*/
+const Navigation = enhace(NavBar);
 
 const Layout = props => {
-  const user = useContext(AuthUserContext);
-  console.log('user: ', user);
-  
   return (
-  <>
-  <Head>
-    <title>My styled page</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-    <script src="/materialize.min.js" />
-  </Head>
-  <NavBar user={user} />
-  <div style={layoutStyle}>
-    {props.children}
-  </div>
-  </>
+    <>
+    <Head>
+      <title>My styled page</title>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+      <script src="/materialize.min.js" />
+    </Head>
+    <Navigation />
+    <div style={layoutStyle}>
+      {props.children}
+    </div>
+    </>
   );
-  }
+}
 
 export default Layout;
