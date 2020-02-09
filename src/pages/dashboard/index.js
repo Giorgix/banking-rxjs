961c05215e3688606649daef3b7fe5b0b9c1d870
Jorge <jorgemdramos@gmail.com>
2020-02-09 14:07:33 +0100

    feat() add withAuthorization hoc to make page private

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,21 +1,14 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Layout from '../../components/Layout';
 
 // Ramda
-import { compose, prop, curry } from 'ramda';
+import { compose } from 'ramda';
 
-// Firebase
-
-// Components
-import Spinner from '../../components/Spinner';
-
-// Redux
-import { connect } from 'react-redux'
-import {requestAccounts, startInterest, stopInterest} from '../../actions'
 
 // HOC
 import {
-    withAuthentication
+    withAuthentication,
+    withAuthorization
 } from '../../hoc';
 
 const Dashboard = ({requestAccounts, startInterest, stopInterest}) => {
@@ -23,28 +16,21 @@ const Dashboard = ({requestAccounts, startInterest, stopInterest}) => {
 
   return (
     <Layout>
-      <div className="login">
-        <main>
-          <p>Dashboard page</p>
-        </main>
-      </div>
+        <div className="login">
+          <main>
+            <p>Dashboard private page</p>
+          </main>
+        </div>
     </Layout>
   )
 };
 
 const enhaceDashboard = compose(
-  withAuthentication
+  withAuthentication,
+  withAuthorization(true)
 )(Dashboard)
 
 enhaceDashboard.getInitialProps = async ({ store, isServer }) => {
-
-    /*const state$ = new StateObservable(new Subject(), store.getState())
-    const resultAction = await rootEpic(
-      of({type: 'REQUEST_ACCOUNTS'}),
-      state$
-    ).toPromise() // we need to convert Observable to Promise
-    console.log('result action: ', resultAction);*/
-
     return { isServer }
 }
 
