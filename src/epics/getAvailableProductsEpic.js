9a6ca0de42c9cc5802c982bdf3e96fcad9cfb2ba
Jorge <jorgemdramos@gmail.com>
2020-03-01 16:52:26 +0100

    feat() add addroductEpic and adapt epcis to new product balances array shape

--- src/epics/getAvailableProductsEpic.js
+++ src/epics/getAvailableProductsEpic.js
@@ -3,29 +3,29 @@ import { ofType } from 'redux-observable';
 import { collectionData } from 'rxfire/firestore';
 import { db } from '../firebase';
 import {
-    LOGGED_OUT,
-    REQUEST_ACCOUNTS,
-    RECEIVE_ACCOUNTS_FULLFILLED,
-    RECEIVE_ACCOUNTS_REJECTED
+    REQUEST_AVAILABLE_PRODUCTS,
+    RECEIVE_AVAILABLE_PRODUCTS_FULLFILLED,
+    RECEIVE_AVAILABLE_PRODUCTS_REJECTED,
+    LOGGED_OUT
 } from '../actions';
 
-const accountsRef = db.collection('products');
+const availableProductsRef = db.collection('available_products');
 
 export default (action$, state$) => action$.pipe(
-        ofType(REQUEST_ACCOUNTS),
+        ofType(REQUEST_AVAILABLE_PRODUCTS),
         delay(Math.trunc(Math.random() * (3000 - 500) + 500)),
         switchMap(action =>
-            collectionData(accountsRef.where("user_id", "==", action.payload), 'id').pipe(
+            collectionData(availableProductsRef, 'id').pipe(
                 timestamp(),
                 map(obj => ({data: [...obj.value], timestamp: obj.timestamp})),
                 map(payload => ({
-                    type: RECEIVE_ACCOUNTS_FULLFILLED,
-                    accounts: payload.data,
+                    type: RECEIVE_AVAILABLE_PRODUCTS_FULLFILLED,
+                    availableProducts: payload.data,
                     receivedAt: payload.timestamp
 
                 })),
                 catchError(payload => [{
-                    type: RECEIVE_ACCOUNTS_REJECTED,
+                    type: RECEIVE_AVAILABLE_PRODUCTS_REJECTED,
                     hasError: true,
                     error: {
                         type: payload.name,
